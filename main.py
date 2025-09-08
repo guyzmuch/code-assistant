4c96c6152970a749d3fdad3e26e087b8ec4d78dc
guyzmuch <guyzmuch@gmail.com>
2025-09-08 21:29:29 +0200

    improving the UI and buttons to copy the result

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import datetime
 import tkinter as tk
 import tkinter.scrolledtext as scrolledtext
+import pyperclip
+
+
+boxed_letters = [
+    "🅰",  # A in box
+    "🅱",  # B in box
+    "🅲",  # C in box
+    "🅳",  # D in box
+    "🅴",  # E in box
+    "🅵",  # F in box
+    "🅶",  # G in box
+    "🅷",  # H in box
+    "🅸",  # I in box
+    "🅹",  # J in box
+    "🅺",  # K in box
+    "🅻",  # L in box
+    "🅼",  # M in box
+    "🅽",  # N in box
+    "🅾",  # O in box
+    "🅿",  # P in box
+    "🆀",  # Q in box
+    "🆁",  # R in box
+    "🆂",  # S in box
+    "🆃",  # T in box
+    "🆄",  # U in box
+    "🆅",  # V in box
+    "🆆",  # W in box
+    "🆇",  # X in box
+    "🆈",  # Y in box
+    "🆉",  # Z in box
+]
+
+copy_symbol = "📋"
 
 def main():
     # Create the main window
@@ -25,10 +58,13 @@ def create_widgets(root):
 
     # Create a frame to hold the buttons
     frame_buttons = tk.Frame(frame)
+    frame_input = tk.Frame(frame)
+    frame_output = tk.Frame(frame)
+    frame_output_buttons = tk.Frame(frame_output)
 
     # create an input text area
     user_input_text_area = scrolledtext.ScrolledText(
-        frame, 
+        frame_input, 
         height=35, 
         width=30,
         wrap=tk.NONE
@@ -36,22 +72,38 @@ def create_widgets(root):
 
     # create an output text area
     user_output_text_area = scrolledtext.ScrolledText(
-        frame, 
+        frame_output, 
         height=35, 
         width=30,
         wrap=tk.NONE
     )
 
-    # create a button
+    # create the actionable buttons
     timestamp_to_iso_date_convertion_button = tk.Button(frame_buttons, text="ISO date convertion", command=lambda: plugin_entrance(timestamp_to_iso_date_convertion, user_input_text_area, user_output_text_area))
     split_by_comma_button = tk.Button(frame_buttons, text="Split by comma", command=lambda: plugin_entrance(split_by_comma, user_input_text_area, user_output_text_area))
+    copy_result_to_input_button = tk.Button(frame_output_buttons, text="⤾ result to input", command=lambda: copy_result_to_input(user_input_text_area, user_output_text_area))
+    copy_result_to_clipboard_button = tk.Button(frame_output_buttons, text=f"{copy_symbol} clipboard", command=lambda: pyperclip.copy(user_output_text_area.get("1.0", "end-1c")))
 
-    user_input_text_area.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    frame_input.pack(side=tk.LEFT)
     frame_buttons.pack(side=tk.LEFT) 
-    user_output_text_area.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    frame_output.pack(side=tk.LEFT)
+
+    user_input_text_area.pack(fill=tk.BOTH, expand=True)
    
     timestamp_to_iso_date_convertion_button.pack()
     split_by_comma_button.pack()
+
+    user_output_text_area.pack(fill=tk.BOTH, expand=True)
+    frame_output_buttons.pack()
+    copy_result_to_input_button.pack(side=tk.LEFT)
+    copy_result_to_clipboard_button.pack(side=tk.LEFT)
+
+# #########
+# app functions
+def copy_result_to_input(input_text_area, output_text_area):
+    output_text = output_text_area.get("1.0", "end-1c")
+    input_text_area.delete("1.0", tk.END)
+    input_text_area.insert("1.0", output_text)
 
 # #########
 # helper functions
@@ -86,7 +138,7 @@ def flatten_and_remove_empty_items(output_list):
         for item in sublist:
             if item:  # Only add non-empty items
                 flattened_list.append(item)
-    return flattened_list
+    return flattened_list 
 
 # #########
 # actions for buttons
